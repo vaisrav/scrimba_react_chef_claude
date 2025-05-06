06a4ede5c9513a53d438dc803cf31eeda441ab10
vaisrav <vaisrav@gmail.com>
2025-05-05 20:23:16 -0400

    event handling in react and introduction to state to add data realtime when using input element.

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,37 @@
+import { useState } from "react";
+
 export default function Main() {
+
+    const [ingredients, setIngredients] = useState([ "chicken", "beef", "pork"]);
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        
+        const newIngredient = e.target.ingredient.value.trim();
+        if (newIngredient) {
+            console.log('Ingredient added!');
+            setIngredients( prev => [...prev, newIngredient])
+            e.target.ingredient.value = "";
+        }
+    }
+
+    const ingredientListItems = ingredients.map(ingredient => (
+        <li key={ingredient}>{ingredient}</li>
+    ))
     return(
         <main>
-            <form className='add-ingredient-form'>
+            <form onSubmit={handleSubmit} className='add-ingredient-form'>
                 <input 
                     aria-label="Add ingredient"
                     type="text"
                     placeholder="e.g. tomato"
+                    name="ingredient"
                 />
-                <button>Add Ingredient</button>
+                <button >Add Ingredient</button>
             </form>
+            <ul>
+                {ingredientListItems}
+            </ul>
         </main>
     )
 }
