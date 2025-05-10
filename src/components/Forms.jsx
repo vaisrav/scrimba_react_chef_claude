1f0c413654d419314e6a5d6fd4b5fa0a9f176d73
vaisrav <vaisrav@gmail.com>
2025-05-10 16:25:53 -0400

    working with action in form and able to get formData

--- src/components/Forms.jsx
+++ src/components/Forms.jsx
@@ -1,14 +1,26 @@
 import React from "react";  
 
 function Forms() {
+
+    function signUp(formData) {
+        const email = formData.get("email");
+        const password = formData.get("password");
+        console.log(email, password);
+    }
+
     return (
         <section>
             <h1>Sign up form</h1>
-            <form>
+            <form action={signUp}>
                 <label htmlFor="email">Email</label>
                 <input type="email" name="email" />
+                <br/>
+
                 <label htmlFor="password">Password</label>
-                <input type="password" name="password" placeholder="Password"/>
+                <input type="password" name="password" />
+                <br/>
+
+                <button type="submit">Submit</button>
             </form>
         </section>
     )
